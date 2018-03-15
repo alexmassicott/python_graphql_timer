1c393e49f700434ea886ff573de0463f83565e77
Alexander  Massicott Jr <bxgoods@aol.com>
2018-03-14 21:05:09 -0400

    no message

--- models.py
+++ models.py
@@ -5,16 +5,22 @@ from pynamodb.attributes import MapAttribute, ListAttribute, UnicodeAttribute, N
 from pynamodb.indexes import AllProjection
 from pynamodb.indexes import GlobalSecondaryIndex
 from pynamodb.models import Model
+from pynamodb.constants import NULL
 from flask import jsonify
 from werkzeug.security import check_password_hash, generate_password_hash
 
 
-def is_password_hash(pwhash):
-    if pwhash.count('$') < 2:
-        return False
-    method, salt, hashval = pwhash.split('$', 2)
+class ModelEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if hasattr(obj, 'attribute_values'):
+            return obj.attribute_values
+        elif isinstance(obj, datetime.datetime):
+            return obj.isoformat()
+        return json.JSONEncoder.default(self, obj)
 
-    return method.startswith('pbkdf2:') and len(method[7:].split(':')) in (1, 2)
+
+def json_dumps(obj):
+    return json.dumps(obj, cls=ModelEncoder)
 
 
 
@@ -29,21 +35,34 @@ class PasswordAttribute(UnicodeAttribute):
 
 
 class SessionMap(MapAttribute):
-    def deserialize(self, value):
-        return self
+    def deserialize(self, values):
+        deserialized_dict = dict()
+        for k in values:
+            v = values[k]
+            attr_value = _get_value_for_deserialize(v)
+            key = self._dynamo_to_python_attr(k)
+            attr_class = self._get_deserialize_class(key, v)
+            if attr_class is None:
+                continue
+            deserialized_value = None
+            if attr_value is not None:
+                deserialized_value = attr_class.deserialize(attr_value)
 
-    def to_dict(self):
-        rval = {}
-        for key in self.attribute_values:
-            print key
-            rval[key] = self.__getattribute__(key)
-        return rval
+            deserialized_dict[key] = json.dumps(deserialized_value)
 
-    id = UnicodeAttribute(null=False)
+        # If this is a subclass of a MapAttribute (i.e typed), instantiate an instance
+        return deserialized_dict
+
+    id = UnicodeAttribute(attr_name='id')
     start_timestamp = NumberAttribute(attr_name='start_timestamp')
     end_timestamp = NumberAttribute(attr_name='end_timestamp')
-    result = UnicodeAttribute(null=False)
+    result = UnicodeAttribute(attr_name='result')
 
+def _get_value_for_deserialize(value):
+    key = next(iter(value.keys()))
+    if key == NULL:
+        return None
+    return value[key]
 
 class User(Model):
     class Meta:
